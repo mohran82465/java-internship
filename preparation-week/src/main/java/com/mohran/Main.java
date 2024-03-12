aaa8ec63cbaaf4ad1f6ee09740d5956fc0973210
mohran <mohamed01098187312@gmail.com>
2024-03-13 00:28:20 +0200

    that's my first task to the internship

---
+++ preparation-week/src/main/java/com/mohran/Main.java
@@ -0,0 +1,8 @@
+package com.mohran;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world from the first week in the internship !!!");
+    }
+}
